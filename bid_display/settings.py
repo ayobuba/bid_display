7ef44445c4044f92de08231bc50ff75b773a7a25
ayobuba <ayobuba@gmail.com>
2017-10-03 20:17:43 +0100

    edited allowed host

--- bid_display/settings.py
+++ bid_display/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_9mci!=)u0#x@1fbx6v6$p_h54#om@cwa*gp=6c*cp$w=$cy-^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.bidkings.ng','www.bidkings.com.ng','ayobuba.pythonanywhere.com']
 
 
 # Application definition
